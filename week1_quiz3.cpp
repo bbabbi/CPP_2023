6e44618ac80484e5d310534b1dabf0e573be185d
bbabbi <mooncheayoung@gmail.com>
2023-03-14 13:08:12 +0900

    week1_quiz3.cpp 수정

--- week1_quiz3.cpp
+++ week1_quiz3.cpp
@@ -3,8 +3,16 @@ using namespace std;
 
 int main(){
   int temp = 0;
-  int nums[10] = { 43, 58, 33, 52, 99, 88, 29, 28, 51, 13 };
+  int nums[10];
   int i=0, j=0;
+
+  cout<<"배열의 원소를 입력하세요. : ";
+  while(i < 10){
+    scanf("%d", &nums[i]);
+    i++;
+  }
+
+  i = 0;
 
   cout<<"데이터 출력: ";
   while(i<10){
