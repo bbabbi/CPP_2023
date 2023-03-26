42bd6716557eb81bb0e49b9920aaf4e8960036f8
bbabbi <mooncheayoung@gmail.com>
2023-03-26 20:00:39 +0900

    아 모르겠다..

--- week3/quiz/week3_quiz3.cpp
+++ week3/quiz/week3_quiz3.cpp
@@ -6,12 +6,13 @@
 #include <iostream>
 using namespace std;
 
-class CoffeeMachine {
+class CoffeeMachine { // CoffeeMachine 클래스
 private : 
   int coffee;
   int water;
   int sugar;
 public :
+  // 문제의 실행결과에서 제공된 메소드의 이름에 맞게 작성한다.
   CoffeeMachine(int coffee, int water, int sugar);
   void drinkEspresso();
   void drinkAmericano();
@@ -21,51 +22,55 @@ public :
 };
 
 CoffeeMachine::CoffeeMachine(int coffee, int water, int sugar)
-{
+{ // 클래스의 멤버변수와 매개변수가 동일하므로, this 포인터를 사용한다.
   this -> coffee = coffee;
   this -> water = water;
   this -> sugar = sugar;
 }
 
 void CoffeeMachine::drinkEspresso()
-{
-  coffee = coffee - 1;
-  water = water - 1;
+{ // 에스프레소 한 잔에는 커와 물이 10씩 소비
+  coffee = coffee - 10;
+  water = water - 10;
 }
 
 void CoffeeMachine::drinkAmericano()
-{
-  coffee = coffee - 1;
-  water = water - 2;
+{ // 아메리카노는 커피 10, 물 20 소비
+  coffee = coffee - 10;
+  water = water - 20;
 }
 
 void CoffeeMachine::drinkSugarCoffee()
-{
-  coffee = coffee - 1;
-  water = water - 2;
-  sugar = sugar - 1;
+{ // 설탕커피는 커피 10, 물 20, 설탕 10 소비
+  coffee = coffee - 10;
+  water = water - 20;
+  sugar = sugar - 10;
 }
 
+// show()는 현재 커피머신의 상태를 출력하는 메소드이다.
 void CoffeeMachine::show()
 {
-  cout<<"커피머신상태, 커피 : "<<coffee<<"\t물 : "<<water<<"\t설탕:"<<sugar<<endl;
+  cout<<"커피머신상태, 커피 : "<<coffee<<"  물 : "<<water<<"  설탕:"<<sugar<<endl;
 }
 
+// fill() 메소드가 실행되면 커피머신이 다시 채워진다.
 void CoffeeMachine::fill()
 {
-  coffee = 10;
-  water = 10;
-  sugar = 10;
+  coffee = 100;
+  water = 100;
+  sugar = 100;
 }
 
 int main(){
-  CoffeeMachine java(1, 2, 1);
+  // 문제에서 제공하는 코드
+  CoffeeMachine java(50, 70, 60);
   java.drinkEspresso();
   java.show();
   java.drinkAmericano();
   java.show();
   java.drinkSugarCoffee();
   java.show();
+  // 커피머신 다시 채우기
   java.fill();
   java.show();
   return 0;
--- week3/quiz/week3_quiz4.cpp
+++ week3/quiz/week3_quiz4.cpp
@@ -1,39 +1,65 @@
+/*
+  2021112037 문채영
+  심화프로그래밍 실습3 과제2 week3_quiz2.cpp
+*/
+
 #include <iostream>
+// 문제의 조건에 맞는 헤더파일 cstdlib와 ctime
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-class Random {
-	int seed = 0;
-public:
+// class Random {
+// 	int seed = 0;
+// public:
+// 	int next();
+// 	int nextInRange(int start, int end);
+// };
+
+// int Random::next() {
+// 	//srand((unsigned int)time(0));
+// 	int n = rand();
+// 	return n;
+// }
+// int Random::nextInRange(int start, int end) {
+// 	//srand((unsigned int)time(0));
+// 	int n = rand() % (end - start + 1) + start;
+// 	return n;
+// }
+
+// int main() {
+// 	Random r;
+// 	cout << "-- 0에서 " << RAND_MAX << "까지의 정수 10개--" << endl;
+// 	for (int i = 0; i < 10; i++) {
+// 		int n = r.next();
+// 		cout << n << ' ';
+// 	}
+// 	cout << endl << endl << "-- 2에서 4까지의 랜덤 정수 10개 --" << endl;
+// 	for (int i = 0; i < 10; i++) {
+// 		int n = r.nextInRange(2, 4);
+// 		cout << n << ' ';
+// 	}
+// 	cout << endl;
+// 	return 0;
+// }
+
+
+class Random{
+public : 
 	int next();
-	int nextInRange(int start, int end);
+	int nextInRange(int a, int b);
+	Random();
 };
 
-int Random::next() {
-	//srand((unsigned int)time(0));
-	int n = rand();
-	return n;
-}
-int Random::nextInRange(int start, int end) {
-	//srand((unsigned int)time(0));
-	int n = rand() % (end - start + 1) + start;
-	return n;
+Random::Random(){
+	srand((unsigned int)time(0));
 }
 
-int main() {
-	Random r;
-	cout << "-- 0에서 " << RAND_MAX << "까지의 정수 10개--" << endl;
-	for (int i = 0; i < 10; i++) {
-		int n = r.next();
-		cout << n << ' ';
-	}
-	cout << endl << endl << "-- 2에서 4까지의 랜덤 정수 10개 --" << endl;
-	for (int i = 0; i < 10; i++) {
-		int n = r.nextInRange(2, 4);
-		cout << n << ' ';
-	}
-	cout << endl;
-	return 0;
+int Random::next(){
+	return rand();
+}
+
+int Random::nextInRange(int a, int b){
+	return rand() % (b - a + 1) + a;
 }
