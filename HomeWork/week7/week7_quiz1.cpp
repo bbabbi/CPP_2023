dd62a4b8380574103df2c54b36f51ab596a7b876
bbabbi <mooncheayoung@gmail.com>
2023-04-30 15:38:36 +0900

    week2 quiz1, quiz2

--- HomeWork/week7/week7_quiz1.cpp
+++ HomeWork/week7/week7_quiz1.cpp
@@ -2,48 +2,48 @@
 // 2021112037 문채영
 
 #include <iostream>
-#include <cstring>
-
+#include <string>
 using namespace std;
 
-class Factory{
-private:
-  char *name; // 저장 공간 확보가 필요한 동적할당 문자 배열
-public:
-  Factory(const char* name); // 생성자
-  Factory(const Factory& f); // 복사생성자
-  ~Factory(); // 소멸자
+//조건 1 : Factory 클래스
+class Factory {
+private : 
+  char *name; // name을 배열로 만들어서 동적할당에 활용
+public :
+  Factory(const char* name);
+  Factory(const Factory& f);
+  ~Factory();
 };
 
-// 생성자
+//생성자
 Factory::Factory(const char* name){
-  int len = (unsigned int)strlen(name); // name길이
-  this->name = new char[len+1]; // \0까지 고려해서 name문자열 공간 할당
-  strcpy(this->name, name); // name에 문자열 복사
-  cout<<"메모리 할당"<<endl;
+  int length = (unsigned int)strlen(name); //length = name의 길이
+  this -> name = new char[length+1]; // name 공간할당, null까지 생각해야함
+  strcpy(this -> name, name); // strcpy를 활용해서 문자열을 복사 => name에 저장
+  cout << "메모리 할당" << endl;
 }
 
-// 복사생성자
-Factory::Factory(const Factory& f){
-  int len = (unsigned int)strlen(f.name); // name의 문자개수
-  this->name = new char[len+1]; // name을 위한 공간 할당해주기
-  strcpy(this->name, f.name); // this->name에 f.name 문자열 복사
-  cout<<this->name<<" 1대 생산"<<endl;
+//복사생성자
+Factory :: Factory(const Factory& f){
+  int length = (unsigned int)strlen(f.name);
+  this -> name = new char[length+1];
+  strcpy(this -> name, f.name); // this -> name에 f.name 문자열을 복사한다.
+  cout << this -> name << " 1대 생산" << endl;
 }
 
-// 소멸자
-Factory::~Factory(){
-  cout<<"메모리 소멸"<<endl;
-  // name이 동적할당되어있다면
-  if(name)
-    delete []name; // 할당해제 진행
+//소멸자
+Factory :: ~Factory(){
+  cout << "메모리 소멸" << endl;
+
+  //name이 동적할당 되어있다면
+  if(name){
+    delete []name; //할당해제 진행
+  }
 }
 
-int main(void)
-{
-  // Factory, "SAMSUNG" 주기
-  Factory fac1 = Factory("SAMSUNG");
-  Factory fac2(fac1);
-  
+int main(void){
+  Factory myfactory = Factory("SAMSUNG");
+  Factory mymyfactory(myfactory);
+
   return 0;
 }
---
+++ HomeWork/week7/week7_quiz2.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#define PI 3.141592
+
+using namespace std;
+
+class Measure {
+private :
+  double r; //반지름
+  double h; //높이
+  double w; // 너비
+  double l; // 한 변의 길이
+public :
+  Measure(){ } //일반 생성자
+
+  Measure(double r){ // 원
+    this -> r = r;
+  }
+
+  Measure(double h, double w){ //직사각형, 삼각형
+    this -> h = h;
+    this -> w = w;
+  }
+
+  void SetHeight(double h){
+    this -> h = h;
+  }
+
+  void SetLine(double l){
+    this -> l = l;
+  }
+
+  //원의 면적
+  void getCircleArea(){
+    cout << "원의 면적은 : " << r * r * PI << endl;
+  }
+
+  //직사각형의 면적
+  void getRectangleArea() {
+    cout << "직사각형의 면적은 : " << w * h << endl;
+  }
+
+  //삼각형의 면적
+  void getTriangleArea(){
+    cout << "삼각형의 면적은 : " << w * h / 2 << endl;
+  }
+
+  //정사각형의 면적
+  void getEqRectArea(){
+    cout << "정사각형의 면적은 : " << l * l << endl;
+  }
+
+  //원뿔의 부피
+  void getConeVol(){
+    cout << "원뿔의 부피는 : " << h * (r * r * PI) / 3 << endl;
+  }
+
+  //구의 부피
+  void getSphereVol(){
+    cout << "구의 부피는 : " << (r * r * r * PI) * 4 / 3 << endl;
+  }
+
+  //원기둥 부피
+  void getCylinderVol(){
+    cout << "실린더의 부피는 : " << h * (r * r * PI) << endl;
+  }
+};
+
+int main(){
+  int choice = 0;
+  double radius; //반지름
+  double width, height; //너비, 높이
+  double length; // 한 변의 길이
+
+  cout << "면적 및 부피 계산기" << endl << endl;
+
+  cout << "1. 원의 면적" << endl;
+  cout << "2. 직사각형의 면적" << endl;
+  cout << "3. 삼각형의 면적" << endl;
+  cout << "4. 정사각형의 면적" << endl;
+  cout << "5. 원뿔의 부피" << endl;
+  cout << "6. 구의 부피" << endl;
+  cout << "7. 실린더의 부피" << endl;
+
+  //사용자로부터 원하는 번호 입력받기
+  cout << "입력 : ";
+  cin >> choice;
+  cout << endl;
+
+  //사용자의 입력에 따른 처리 (조건 : switch문)
+  switch(choice){
+  case 1 : { // 1. 원의 면적
+    //사용자로부터 반지름 입력받기
+    cout << "원의 반지름 입력 : ";
+    cin >> radius;
+    cout << endl;
+
+    Measure *c = new Measure(radius);//동적생성
+    c -> getCircleArea();
+    delete c;//할당해제
+
+    break;
+  }
+
+  case 2 : {//2. 직사각형의 면적
+    cout << "직사각형의 높이, 너비 입력 : ";
+    cin >> height >> width;
+    cout << endl;
+
+    Measure *r = new Measure(height, width); //동적생성
+    r -> getRectangleArea();
+    delete r;//할당해제
+
+    break;
+  }
+
+  case 3 : {//3. 삼각형의 면적
+    cout << "삼각형의 높이, 너비 입력 : ";
+    cin >> height >> width;
+    cout << endl;
+
+    Measure *t = new Measure(height, width); // 동적생성
+    t -> getTriangleArea();
+    delete t; //할당해제
+  }
+
+  case 4 : {//4. 정사각형의 면적
+    cout << "정사각형 한 변의 길이 입력 : ";
+    cin >> length;
+    cout << endl;
+
+    Measure *eq = new Measure(); // 동적생성
+    eq -> SetLine(length);
+    eq -> getEqRectArea();
+    delete eq; //할당해제
+    break;
+  }
+
+  case 5 : { //5. 원뿔의 부피
+    cout << "원뿔의 반지름, 높이 입력 : ";
+    cin >> radius >> height;
+    cout << endl;
+
+    Measure *cone = new Measure(radius); // 동적생성
+    cone -> SetHeight(height);
+    cone -> getSphereVol();
+    delete cone; // 할당해제
+    break;
+  }
+
+  case 6 : { // 6. 구의 부피
+    cout << "구의 반지름 입력 : ";
+    cin >> radius;
+    cout << endl;
+
+    Measure *sp = new Measure(radius);
+    sp -> getSphereVol();
+    delete sp; //할당해제
+    break;
+  }
+
+  case 7 : { //7. 실린더의 부피
+    cout << "실린더의 반지름, 높이 입력 : ";
+    cin >> radius >> height;
+    cout << endl;
+
+    Measure *cyl = new Measure(radius); //동적생성
+    cyl -> SetHeight(height);
+    cyl -> getCylinderVol();
+    delete cyl; //할당해제
+    break;
+  }
+  default : //1~7이 아닌 번호가 입력되었을 경우
+    cout << "1~7번만을 입력할 수 있습니다. 그 외의 번호가 입력되었으므로, 프로그램이 종료됩니다."<<endl;
+  }
+
+  return 0;
+
+}
